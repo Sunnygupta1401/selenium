a34e4a4020f506357b340c5051380d329b4ce514
Sunny Gupta <sunnygupta@Sunnys-MacBook-Pro.local>
2019-03-09 19:39:36 -0500

    jj

---
+++ src/test/java/selanium/NewTest.java
@@ -0,0 +1,65 @@
+package selanium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	
+	WebDriver driver;
+  @Test
+  public void f() {
+	  
+	  
+	  try {
+			// TODO Auto-generated method stub
+			driver.get("https://www.amazon.com/");
+
+		  //driver.findElement(By.className("NKcBbdsdsdd")).click();
+			
+		  
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.fail();
+		}
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  
+  @BeforeTest
+  public void beforeTest()
+  {
+	  System.setProperty("webdriver.chrome.driver", "/Users/sunnygupta/Downloads/chromedriver");
+		 driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+  }
+  
+  @AfterTest
+  public void afterTest()
+  {
+	  
+	  driver.close();	  
+  }
+  }
+
